e9d156829b0ea50658298911e50dd5c86487521f
yann <yann.boisselier@gmail.com>
2017-08-10 20:20:39 +0200

    cleaning

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,6 @@
   <nav>
     <ul>
       <li><nuxt-link to="/">Homepage</nuxt-link></li>
-      <li><nuxt-link to="/contact">Contact</nuxt-link></li>
       <li><nuxt-link to="/blog">Blog</nuxt-link></li>
     </ul>
   </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
-        komuneco
+        Drupal 8 graphQL demo blog public API client
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a class="button is-primary" href="https://github.com/nyl-auster/drupal8-graphql-demo-blog-docs" target="_blank">Documentation</a>
       </div>
     </div>
   </section>
